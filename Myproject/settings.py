6ccb56fd015363da122cb6656c1ac9d80f829e0d
lybu-4612 <lybu4612@gmail.com>
2022-06-09 17:46:38 +0530

    allowed host

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)  #True or false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-qptapqmw.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
